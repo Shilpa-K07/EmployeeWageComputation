38e733032fb4ab980be847637eca08e6f66241b0
shilpa <shilpa07udupi@gmail.com>
2020-09-03 19:59:11 +0530

    Adding employee attendence check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,4 +1,17 @@
+#!/bin/bash -x
 echo welcome to Employee Wage Computation Problem !
-echo welcome
-echo hello
-echo hi
+
+isFullTime=1
+isPartTime=2
+randomCheck=$((RANDOM%3))
+
+if [ $isFullTime -eq $randomCheck ]
+then
+        echo "Employee is present and Employee is full time"
+
+elif [ $isPartTime -eq $randomCheck ]
+then
+        echo "Employee is present and Employee is part time"
+else
+        echo "Employee is absent"
+fi
