75d361dbc43263540987ae10d9d6ebd7c16ab0d1
shilpa <shilpa07udupi@gmail.com>
2020-09-04 19:17:44 +0530

    Adding Switch case to find daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,10 +1,30 @@
 #!/bin/bash -x
 echo welcome to Employee Wage Computation Problem !
+randomCheck=$((RANDOM%2))
 
-randomCheck=$((RANDOM%3))
+if [ $randomCheck -eq 1 ]
+then
+        echo "Employee is present"
+
+elif [ $randomCheck -eq 0 ]
+then
+        echo "Employee is absent"
+fi
 
 empWagePerHour=20
-case $randomCheck in
+FullDayHour=8
+dailyWage=$(($FullDayHour*$empWagePerHour))
+echo Daily Employee Wage for Full time Employee is $dailyWage
+
+empWagePerHour=20
+PartTimeHour=4
+dailyWage=$(($PartTimeHour*$empWagePerHour))
+echo Daily Employee Wage for Parttime Employee is $dailyWage
+
+randomCheck1=$((RANDOM%3))
+
+empWagePerHour=20
+case $randomCheck1 in
 		1)
 		empHrs=8
 		;;
