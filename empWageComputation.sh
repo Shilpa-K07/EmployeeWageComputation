b8c9a7de33073c9fbb6c7de9b236295901e26ba0
Shilpa-K07 <70196456+Shilpa-K07@users.noreply.github.com>
2020-09-04 13:00:05 +0530

    Update empWageComputation.sh

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,9 +11,3 @@ elif [ $randomCheck -eq 0 ]
 then
         echo "Employee is absent"
 fi
-
-empWagePerHour=20
-FullDayHour=8
-dailyWage=$(($FullDayHour*$empWagePerHour))
-echo Daily Employee Wage for Full time Employee is $dailyWage
-echo welcome
