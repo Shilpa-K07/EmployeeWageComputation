3af7573efd8c5e586fc76809d27d2c4d8e57dc7c
shilpa <shilpa07udupi@gmail.com>
2020-09-04 21:09:30 +0530

    adding function to get work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -61,10 +61,8 @@ done
 
 echo "Monthly wage using condition is: " $MonthlyWage
 
-echo Using function to get work hours 
+echo Ading function to get work hours 
 
-randomCheck1=$((RANDOM%3))
-empWagePerHour=20
 function getWorkingHours
 {
 case $randomCheck1 in
@@ -82,26 +80,4 @@ echo $empHrs
 }
 
 empHrs="$(getWorkingHours $randomCheck1)"
-dailyWage=$(($empHrs*$empWagePerHour))
-
-echo Employee Daily Wage is $dailyWage
-
-
-echo "Calculating Wages for a Month"
-
-totalWorkingDays=20
-
-MonthlyWage=$(($dailyWage*$totalWorkingDays))
-
-echo employee wage for a month $MonthlyWage
-
-echo "Calculating wages for a month using condition of totalworking days of 20 is reached"
-totalWorkingDays=0
-MonthlyWage=0
-while [[ totalWorkingDays -lt 20 ]]
-do
-	MonthlyWage=$(($MonthlyWage+$dailyWage))
-	totalWorkingDays=$(($totalWorkingDays+1))
-done
-
-echo "Monthly wage using condition is: " $MonthlyWage
+ echo work hours is $empHrs
