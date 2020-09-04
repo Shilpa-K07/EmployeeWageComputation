6d887bef94f7a6a79de1c741f8ecc4764afc68db
shilpa <shilpa07udupi@gmail.com>
2020-09-04 19:57:09 +0530

    Calculating Wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,40 +1,23 @@
 #!/bin/bash -x
 echo welcome to Employee Wage Computation Problem !
-randomCheck=$((RANDOM%2))
-
-if [ $randomCheck -eq 1 ]
-then
-        echo "Employee is present"
-
-elif [ $randomCheck -eq 0 ]
-then
-        echo "Employee is absent"
-fi
-
-empWagePerHour=20
-FullDayHour=8
-dailyWage=$(($FullDayHour*$empWagePerHour))
-echo Daily Employee Wage for Full time Employee is $dailyWage
-
-empWagePerHour=20
-PartTimeHour=4
-dailyWage=$(($PartTimeHour*$empWagePerHour))
-echo Daily Employee Wage for Parttime Employee is $dailyWage
+echo "Calculating Wages for a Month"
 
 randomCheck1=$((RANDOM%3))
 
+totalWorkingDays=20
 empWagePerHour=20
 case $randomCheck1 in
-		1)
-		empHrs=8
-		;;
-		2)
-		empHrs=4
-		;;
-		*)
-		empHrs=0
-		;;
+                1)
+                empHrs=8
+                ;;
+                2)
+                empHrs=4
+                ;;
+                *)
+                empHrs=0
+                ;;
 esac
 dailyWage=$(($empHrs*$empWagePerHour))
+MonthlyWage=$(($dailyWage*$totalWorkingDays))
 
-echo Employee Daily Wage is $dailyWage
+echo employee wage for a month $MonthlyWage
