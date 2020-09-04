cdbf02ab553e841da9f107f7d9ef4124fcc9111d
shilpa <shilpa07udupi@gmail.com>
2020-09-04 20:26:46 +0530

    Monthly wage till total working days is reached

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,28 +1,9 @@
 #!/bin/bash -x
 echo welcome to Employee Wage Computation Problem !
-randomCheck=$((RANDOM%2))
-
-if [ $randomCheck -eq 1 ]
-then
-        echo "Employee is present"
-
-elif [ $randomCheck -eq 0 ]
-then
-        echo "Employee is absent"
-fi
-
-empWagePerHour=20
-FullDayHour=8
-dailyWage=$(($FullDayHour*$empWagePerHour))
-echo Daily Employee Wage for Full time Employee is $dailyWage
-
-empWagePerHour=20
-PartTimeHour=4
-dailyWage=$(($PartTimeHour*$empWagePerHour))
-echo Daily Employee Wage for Parttime Employee is $dailyWage
 
 randomCheck1=$((RANDOM%3))
 
+totalWorkingDays=20
 empWagePerHour=20
 case $randomCheck1 in
                 1)
@@ -36,17 +17,6 @@ case $randomCheck1 in
                 ;;
 esac
 dailyWage=$(($empHrs*$empWagePerHour))
-
-echo Employee Daily Wage is $dailyWage
-
-
-echo "Calculating Wages for a Month"
-
-totalWorkingDays=20
-
-MonthlyWage=$(($dailyWage*$totalWorkingDays))
-
-echo employee wage for a month $MonthlyWage
 
 echo "Calculating wages for a month using condition of titalworking days of 20 is reached"
 totalWorkingDays=0
