06329099d395253d45d5a244c423accf40240585
shilpa <shilpa07udupi@gmail.com>
2020-09-04 16:07:19 +0530

    Adding daily wage of employee

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,19 +1,7 @@
 #!/bin/bash -x
 echo welcome to Employee Wage Computation Problem !
 
-randomCheck=$((RANDOM%2))
-
-if [ $randomCheck -eq 1 ]
-then
-        echo "Employee is present"
-
-elif [ $randomCheck -eq 0 ]
-then
-        echo "Employee is absent"
-fi
-
 empWagePerHour=20
 FullDayHour=8
 dailyWage=$(($FullDayHour*$empWagePerHour))
 echo Daily Employee Wage for Full time Employee is $dailyWage
-echo welcome
