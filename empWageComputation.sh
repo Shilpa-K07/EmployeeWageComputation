add80e2c5d5919f8f16673f04fe605b21682f093
shilpa <shilpa07udupi@gmail.com>
2020-09-04 20:47:10 +0530

    Using function to get work hours to calculate wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,30 +1,12 @@
 #!/bin/bash -x
 echo welcome to Employee Wage Computation Problem !
 
-randomCheck=$((RANDOM%2))
-
-if [ $randomCheck -eq 1 ]
-then
-        echo "Employee is present"
-
-elif [ $randomCheck -eq 0 ]
-then
-        echo "Employee is absent"
-fi
-
-empWagePerHour=20
-FullDayHour=8
-dailyWage=$(($FullDayHour*$empWagePerHour))
-echo Daily Employee Wage for Full time Employee is $dailyWage
-
-empWagePerHour=20
-PartTimeHour=4
-dailyWage=$(($PartTimeHour*$empWagePerHour))
-echo Daily Employee Wage for Parttime Employee is $dailyWage
+echo Using function to get work hours 
 
 randomCheck1=$((RANDOM%3))
-
 empWagePerHour=20
+function getWorkingHours
+{
 case $randomCheck1 in
                 1)
                 empHrs=8
@@ -36,6 +18,10 @@ case $randomCheck1 in
                 empHrs=0
                 ;;
 esac
+echo $empHrs
+}
+
+empHrs="$(getWorkingHours $randomCheck1)"
 dailyWage=$(($empHrs*$empWagePerHour))
 
 echo Employee Daily Wage is $dailyWage
