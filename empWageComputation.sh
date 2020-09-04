30a3a4fa7a07e22277ba7ae3f13f858256ba2f99
shilpa <shilpa07udupi@gmail.com>
2020-09-04 22:29:00 +0530

    store day with daily wage and total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,67 +1,7 @@
 #!/bin/bash  
 echo welcome to Employee Wage Computation Problem !
 
-randomCheck=$((RANDOM%2))
-
-if [ $randomCheck -eq 1 ]
-then
-        echo "Employee is present"
-
-elif [ $randomCheck -eq 0 ]
-then
-        echo "Employee is absent"
-fi
-
-empWagePerHour=20
-FullDayHour=8
-dailyWage=$(($FullDayHour*$empWagePerHour))
-echo Daily Employee Wage for Full time Employee is $dailyWage
-
-empWagePerHour=20
-PartTimeHour=4
-dailyWage=$(($PartTimeHour*$empWagePerHour))
-echo Daily Employee Wage for Parttime Employee is $dailyWage
-
-randomCheck1=$((RANDOM%3))
-
-echo randomCheck is $randomCheck1
-empWagePerHour=20
-case $randomCheck1 in
-                1)
-                empHrs=8
-                ;;
-                2)
-                empHrs=4
-                ;;
-                *)
-                empHrs=0
-                ;;
-esac
-dailyWage=$(($empHrs*$empWagePerHour))
-
-echo Employee Daily Wage is $dailyWage
-
-
-echo "Calculating Wages for a Month"
-
-totalWorkingDays=20
-
-MonthlyWage=$(($dailyWage*$totalWorkingDays))
-
-echo employee wage for a month $MonthlyWage
-
-echo "Calculating wages for a month using condition of totalworking days of 20 is reached"
-totalWorkingDays=0
-MonthlyWage=0
-while [[ totalWorkingDays -lt 20 ]]
-do
-	MonthlyWage=$(($MonthlyWage+$dailyWage))
-	totalWorkingDays=$(($totalWorkingDays+1))
-done
-
-echo "Monthly wage using condition is: " $MonthlyWage
-
-echo Ading function to get work hours 
+echo storing daily wage and total wage
 
 function getWorkingHours
 {
@@ -79,10 +19,6 @@ esac
 echo $empHrs
 }
 
-echo workinh hours $empHrs
-
-echo storing daily wage and total wage
-
 totalWOrkingDays=0
 totalWage=0
 empRatePerHour=20
@@ -96,5 +32,7 @@ randomcheck=$((RANDOM%3))
                 dailyWageArray[$totalWOrkingDays]=$dailywage
                 totalWage=$(($totalWage+$dailywage))
 done
+echo " Day " ${!dailyWageArray[@]}
 echo " DailyWage " ${dailyWageArray[@]}
 echo " Total Wage " $totalWage
+
